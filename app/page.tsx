d4658d4a73c454b961b48c6a1d35ff0ecf678e91
Sarah Holden <sarahbethholden@gmail.com>
2024-01-06 15:54:13 -0800

    Updates for score board

--- app/page.tsx
+++ app/page.tsx
@@ -37,15 +37,13 @@ const initialPlayers = [
 
 export function Game() {
   const [players, setPlayers] = useState(initialPlayers);
-  const [turnNumber, setTurnNumber] = useState(1);
+  const [turnNumber, setTurnNumber] = useState(0);
   const currentPlayer = players[turnNumber];
 
   const onSaveScore = (score: number) => {
     let playersDupe = [...players];
     playersDupe[turnNumber].score += score;
     setPlayers(playersDupe);
-    console.log(score);
-    console.log(players);
     switchTurn();
   };
 
@@ -79,7 +77,14 @@ export function PlayersList({
         {players.map((player) => {
           return (
             <li key={player.id} className="flex border-black border-t-2">
-              {currentPlayer.id === player.id && `Dice!`}
+              {currentPlayer.id === player.id && (
+                <Image
+                  src="/dice.svg"
+                  width={30}
+                  height={30}
+                  alt="Picture of the author"
+                />
+              )}
               <p className="flex-auto">{player.name}</p>
               <p className="ml-auto">{player.score}</p>
             </li>
@@ -97,7 +102,9 @@ export function AddScore({
   currentPlayer: Player;
   onSaveScore: any;
 }) {
-  const [score, setScore] = useState(`${currentPlayer.score}`);
+  const [score, setScore] = useState(
+    `${currentPlayer.score !== 0 ? currentPlayer.score.toString() : ""}`
+  );
 
   const handleScoreSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -107,21 +114,77 @@ export function AddScore({
     }
   };
 
+  const handleNumberClick = (e: React.MouseEvent<HTMLFieldSetElement>) => {
+    const newValue = (e.target as HTMLButtonElement).value;
+
+    if (newValue === undefined) return;
+
+    const newScore =
+      newValue === "X"
+        ? score.substring(0, score.length - 1)
+        : score + newValue;
+    setScore(newScore);
+  };
+
+  // Event delegation + value
+
   return (
-    <form onSubmit={(e) => handleScoreSubmit(e)}>
+    <form onSubmit={(e) => handleScoreSubmit(e)} className="add-board">
       <label htmlFor="score">Score</label>
-      <input
-        type="number"
-        id="score"
-        name="score"
-        placeholder="0"
-        required
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          setScore(e.target.value);
-        }}
-        value={score}
-      />
-      <button type="submit">Save Points</button>
+      <fieldset
+        className="add-board__buttons"
+        onClick={(e) => handleNumberClick(e)}
+      >
+        <button type="button" value="1">
+          1
+        </button>
+        <button type="button" value="2">
+          2
+        </button>
+        <button type="button" value="3">
+          3
+        </button>
+        <button type="button" value="4">
+          4
+        </button>
+        <button type="button" value="5">
+          5
+        </button>
+        <button type="button" value="6">
+          6
+        </button>
+        <button type="button" value="7">
+          7
+        </button>
+        <button type="button" value="8">
+          8
+        </button>
+        <button type="button" value="9">
+          9
+        </button>
+        <button type="button" value="0">
+          0
+        </button>
+        <button type="button" value="X">
+          &lt;
+        </button>
+      </fieldset>
+      <div className="add-board__submit-row">
+        <input
+          type="number"
+          id="score"
+          name="score"
+          placeholder="0"
+          required
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setScore(e.target.value);
+          }}
+          value={score}
+        />
+        <button type="submit" className="btn btn--primary">
+          Save Points
+        </button>
+      </div>
     </form>
   );
 }
